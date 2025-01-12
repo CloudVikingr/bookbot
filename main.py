f2488f31c99cd627df8332575251bac3b99ea104
Jason Wallace <jason@cloudviking.ca>
2025-01-11 23:41:45 -0800

    read frankenstein and print results to console

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+# main.py
+
+def main():
+    """The main entry point of the program."""
+    path = "books/frankenstein.txt"
+
+    file_contents = ""
+
+    with open(path) as f:
+        file_contents = f.read()
+
+    print(file_contents)
+
+if __name__ == "__main__":
+    main()
