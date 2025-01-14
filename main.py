cf6791725e65088c5c88ff8b1fcad1171cafb74e
Jason Wallace <jason@cloudviking.ca>
2025-01-13 22:22:34 -0800

    create report

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 # main.py
+import string
+
+def Get_Book(path):
+
+    file_contents = ""
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def Print_Book(book):
+    print(book)
+
+def Count_Words(book):
+    words = book.split()
+    return len(words)
+
+def Count_Characters(book):
+    alphabet_dict = {letter: 0 for letter in string.ascii_lowercase} 
+    for char in book.lower():
+        if char in alphabet_dict:
+            alphabet_dict[char] += 1
+    return alphabet_dict
+
+def sort_on(dict):
+    return dict["num"]
 
 def main():
     """The main entry point of the program."""
     path = "books/frankenstein.txt"
 
-    file_contents = ""
+    book = Get_Book(path)
+    print(f"--- Begin report of {path}")
+    print(f"{Count_Words(book)} words found")
+    char_count = Count_Characters(book)
 
-    with open(path) as f:
-        file_contents = f.read()
+    # Convert to a list of dictionaries
+    list_of_dicts = [{"name": k, "num": v} for k, v in char_count.items()]
 
-    print(file_contents)
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    for dict in list_of_dicts:
+        print(dict)
+    print("End of report")
 
 if __name__ == "__main__":
     main()
